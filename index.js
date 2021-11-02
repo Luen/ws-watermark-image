af0d253e68055cc0ba790f0070115b5707f39789
L <luenwarneke@gmail.com>
2021-11-02 17:48:17 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const Fs = require('fs');
+const Jimp = require("jimp");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('*', function(req, res) {
+
+  console.log(req.url);
+  const ORIGINAL_IMAGE = "https://wanderstories.space/" + req.url;
+
+  const EXTENSION = ORIGINAL_IMAGE.split('.').pop().toLowerCase();
+
+  const LOGO = "https://wanderstories.space/content/images/size/w1000/2016/06/Wander-Stories-logo.jpg";
+
+  const LOGO_MARGIN_PERCENTAGE = 5;
+
+  const FILENAME = ORIGINAL_IMAGE.split('/').pop()
+
+  const ACCEPTED = [
+    'jpg',
+    'jpeg',
+    'png'
+  ]
+
+  if (ACCEPTED.indexOf(EXTENSION) >= 0) { // If an accepted fileextensinon
+
+    const PATH = __dirname + '/images/' + FILENAME;
+    console.log(PATH)
+
+    if (Fs.existsSync(PATH)) {
+
+        //res.send('File Exists ' + FILENAME + '!'); // file exists, send file
+        res.sendFile(PATH)
+
+    } else { // generate file
+
+      const main = async () => {
+        const [image, logo] = await Promise.all([
+          Jimp.read(ORIGINAL_IMAGE),
+          Jimp.read(LOGO)
+        ]);
+
+        logo.resize(image.bitmap.width / 10, Jimp.AUTO);
+
+        const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+        const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+
+        const X = image.bitmap.width - logo.bitmap.width - xMargin;
+        const Y = image.bitmap.height - logo.bitmap.height - yMargin;
+
+        return image.composite(logo, X, Y, [
+          {
+            mode: Jimp.BLEND_SCREEN,
+            opacitySource: 0.1,
+            opacityDest: 1
+          }
+        ]);
+      };
+
+      main().then(image => image.writeAsync('images/' + FILENAME)).then(() => {
+        res.sendFile(PATH)
+      })
+    }
+
+  } else { // not acceptable fileformat
+    res.status(404).send('Not Found !!!');
+  }
+
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
