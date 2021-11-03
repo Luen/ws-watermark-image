27246288adcf37ff0a6a75298ee5697e11cce626
L <luenwarneke@gmail.com>
2021-11-04 09:57:20 +1000

    escape req.url & add comments

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const Fs = require('fs');
-const Jimp = require("jimp");
+const express = require('express')
+const Fs = require('fs')
+const Jimp = require("jimp")
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
 app.get('*', function(req, res) {
 
   //const FILENAME = ORIGINAL_IMAGE.split('/').pop()
-  const FILENAME = req.url
-  console.log(FILENAME);
+  const FILENAME = escape(req.url)
+  console.log(FILENAME)
 
-  const ORIGINAL_IMAGE = "https://wanderstories.space" + FILENAME;
+  const ORIGINAL_IMAGE = "https://wanderstories.space" + FILENAME
 
-  const EXTENSION = ORIGINAL_IMAGE.split('.').pop().toLowerCase();
+  const FILE_EXTENSION = ORIGINAL_IMAGE.split('.').pop().toLowerCase()
 
-  const LOGO = "https://wanderstories.space/content/images/size/w1000/2016/06/Wander-Stories-logo.jpg";
+  //const LOGO = "https://wanderstories.space/content/images/size/w1000/2016/06/Wander-Stories-logo.jpg"
+  const LOGO = "https://wanderstories.space/content/images/size/w1000/2021/11/Wanderstories-publication-logo.png"
 
-  const LOGO_MARGIN_PERCENTAGE = 5;
+  const LOGO_MARGIN_PERCENTAGE = 5
 
   const ACCEPTED = [
     'jpg',
@@ -25,31 +26,34 @@ app.get('*', function(req, res) {
     'png'
   ]
 
-  if (ACCEPTED.indexOf(EXTENSION) >= 0 && FILENAME.split('/')[1] == 'content') { // If an accepted fileextensinon
+   // If an accepted file extensinon && path is /content/images
+  if (ACCEPTED.indexOf(FILE_EXTENSION) >= 0 && FILENAME.split('/')[1] == 'content' && FILENAME.split('/')[2] == 'images') {
 
-    const PATH = __dirname + FILENAME.replace('/content','');
+    // Store on server in images folder (not content/images)
+    const PATH = __dirname + FILENAME.replace('/content','')
     console.log(PATH)
 
+    // Check if file exists on server
     if (Fs.existsSync(PATH)) {
-
-        //res.send('File Exists ' + FILENAME + '!'); // file exists, send file
+        // file already exists, send file
+        //res.send('File Exists ' + FILENAME + '!')
         res.sendFile(PATH)
 
-    } else { // generate file
+    } else { // file does not exist, generate watermarked image
 
       const main = async () => {
         const [image, logo] = await Promise.all([
           Jimp.read(ORIGINAL_IMAGE),
           Jimp.read(LOGO)
-        ]);
+        ])
 
-        logo.resize(image.bitmap.width / 10, Jimp.AUTO);
+        logo.resize(image.bitmap.width / 10, Jimp.AUTO)
 
-        const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
-        const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+        const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100
+        const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100
 
-        const X = image.bitmap.width - logo.bitmap.width - xMargin;
-        const Y = image.bitmap.height - logo.bitmap.height - yMargin;
+        const X = image.bitmap.width - logo.bitmap.width - xMargin
+        const Y = image.bitmap.height - logo.bitmap.height - yMargin
 
         return image.composite(logo, X, Y, [
           {
@@ -57,19 +61,20 @@ app.get('*', function(req, res) {
             opacitySource: 0.1,
             opacityDest: 1
           }
-        ]);
-      };
+        ])
+      }
 
+      // write file to server and send file
       main().then(image => image.writeAsync(PATH)).then(() => {
         res.sendFile(PATH)
       })
     }
 
   } else { // not acceptable fileformat
-    res.status(404).send('Not Found !!!');
+    res.status(404).send('Not Found !!!')
   }
 
-});
+})
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+app.listen(port)
+console.log('Server started at http://localhost:' + port)
