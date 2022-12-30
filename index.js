a3da0a92875125943c19f9a3a99ff66f0a20babb
L <luenwarneke@gmail.com>
2022-12-31 09:51:48 +1000

    added rate limit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit')
 const Fs = require('fs')
 const Jimp = require("jimp")
 
 const app = express()
 const port = process.env.PORT || 8080
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 200, // Limit each IP to 200 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 app.get('/', async (req, res, next) => {
   res.writeHead(200, {
