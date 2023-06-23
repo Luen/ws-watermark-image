3bd1a9d77e59b40a504e3993b8059e212e866ff9
L <Luen@users.noreply.github.com>
2023-06-23 10:45:52 +1000

    CWE-23 Path Traversal CWE-918 Server-Side Request Forgery (SSRF)

--- sharp.js
+++ sharp.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
-const Fs = require('fs');
-const Sharp = require('sharp');
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const Sharp = require('sharp');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -25,16 +26,22 @@ app.get('/', async (req, res, next) => {
 
 app.get('*', async (req, res) => {
     try {
-        const segments = req.url.split('/');
+        const segments = req.url.split('/').filter(segment => segment);
         
-        if (segments.length < 4 || segments[1] !== 'content' || segments[2] !== 'images') {
+        if (segments.length < 3 || segments[0] !== 'content' || segments[1] !== 'images') {
             return res.status(404).send('Not Found');
         }
 
-        const encodedPath = segments.map(segment => encodeURIComponent(segment)).join('/');
-        const originalImage = new URL(encodedPath, 'https://wanderstories.space');
+        const encodedSegments = segments.map(segment => encodeURIComponent(segment));
+        const encodedPath = encodedSegments.join('/');
         
-        const fileExtension = originalImage.pathname.split('.').pop().toLowerCase();
+        const originalImageUrl = new URL(`https://wanderstories.space/${encodedPath}`);
+        
+        if (originalImageUrl.origin !== 'https://wanderstories.space') {
+            return res.status(400).send('Invalid URL');
+        }
+
+        const fileExtension = originalImageUrl.pathname.split('.').pop().toLowerCase();
         const accepted = ['jpg', 'jpeg', 'png'];
         
         if (!accepted.includes(fileExtension)) {
@@ -42,17 +49,22 @@ app.get('*', async (req, res) => {
         }
 
         const logoPath = path.join(__dirname, 'Wanderstories-logo.png');
-        const imagePath = path.join(__dirname, ...segments.slice(1));
+        const imagePath = path.normalize(path.join(__dirname, ...encodedSegments));
+        const relativePath = path.relative(__dirname, imagePath);
+
+        if (relativePath.startsWith('..') || path.isAbsolute(relativePath)) {
+            return res.status(400).send('Invalid path');
+        }
 
         if (fs.existsSync(imagePath)) {
             return res.sendFile(imagePath);
         }
 
-        const imageResponse = await axios.get(originalImage.href, { responseType: 'arraybuffer' });
-        
+        const imageResponse = await axios.get(originalImageUrl.href, { responseType: 'arraybuffer' });
+
         const imageBuffer = imageResponse.data;
         const metadata = await sharp(imageBuffer).metadata();
-        
+
         const resizedLogoBuffer = await sharp(logoPath)
             .resize({ width: Math.round(metadata.width / 5) })
             .toBuffer();
@@ -71,7 +83,7 @@ app.get('*', async (req, res) => {
         const directoryPath = path.dirname(imagePath);
         fs.mkdirSync(directoryPath, { recursive: true });
         fs.writeFileSync(imagePath, outputBuffer);
-        
+
         return res.sendFile(imagePath);
 
     } catch (err) {
