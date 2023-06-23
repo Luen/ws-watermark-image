27542c303e515f4f145daabff12f7703c5a11f68
L <Luen@users.noreply.github.com>
2023-06-23 10:41:41 +1000

    Fix CWE-918 Server-Side Request Forgery (SSRF)

--- sharp.js
+++ sharp.js
@@ -23,67 +23,63 @@ app.get('/', async (req, res, next) => {
     res.end('Wanderstories Image Watermarker');
 });
 
-app.get('*', async (req, res, next) => {
-    function encodePath(path) {
-        return path.split('/').map(segment => encodeURIComponent(segment)).join('/');
-    }    
-    const FILENAME = encodePath(req.url);
-    const ORIGINAL_IMAGE = 'https://wanderstories.space' + FILENAME;
-    const FILE_EXTENSION = ORIGINAL_IMAGE.split('.').pop().toLowerCase();
-    const LOGO = __dirname + '/Wanderstories-logo.png';
-    const ACCEPTED = ['jpg', 'jpeg', 'png'];
-    
-    if (
-        ACCEPTED.indexOf(FILE_EXTENSION) >= 0 &&
-        FILENAME.split('/')[1] === 'content' &&
-        FILENAME.split('/')[2] === 'images'
-    ) {
-        const PATH = __dirname + FILENAME.replace('/content', '');
-        console.log(PATH);
+app.get('*', async (req, res) => {
+    try {
+        const segments = req.url.split('/');
         
-        if (Fs.existsSync(PATH)) {
-            res.sendFile(PATH);
-        } else {
-            axios
-                .get(ORIGINAL_IMAGE, { responseType: 'arraybuffer' })
-                .then((imageResponse) => {
-                    const imageBuffer = imageResponse.data;
-
-                    Sharp(imageBuffer)
-                        .metadata()
-                        .then(({ width }) => {
-                            return Sharp(LOGO)
-                                .resize({ width: Math.round(width / 5) })
-                                .toBuffer();
-                        })
-                        .then((resizedLogoBuffer) => {
-                            return Sharp(imageBuffer)
-                                .composite([
-                                    {
-                                        input: resizedLogoBuffer,
-                                        gravity: 'centre',
-                                        blend: 'overlay',
-                                    },
-                                ])
-                                .jpeg({ quality: 60 })
-                                .toBuffer();
-                        })
-                        .then((outputBuffer) => {
-                            const directoryPath = PATH.substring(0, PATH.lastIndexOf("/"));
-                            Fs.mkdirSync(directoryPath, { recursive: true });
-                            Fs.writeFileSync(PATH, outputBuffer);
-                            res.sendFile(PATH);
-                        })
-                })
-                .catch((err) => {
-                    console.error(err);
-                    res.status(500).send('Server Error');
-                });
+        if (segments.length < 4 || segments[1] !== 'content' || segments[2] !== 'images') {
+            return res.status(404).send('Not Found');
         }
-    } else {
-        res.status(404).send('Not Found !!!');
+
+        const encodedPath = segments.map(segment => encodeURIComponent(segment)).join('/');
+        const originalImage = new URL(encodedPath, 'https://wanderstories.space');
+        
+        const fileExtension = originalImage.pathname.split('.').pop().toLowerCase();
+        const accepted = ['jpg', 'jpeg', 'png'];
+        
+        if (!accepted.includes(fileExtension)) {
+            return res.status(400).send('Invalid file type');
+        }
+
+        const logoPath = path.join(__dirname, 'Wanderstories-logo.png');
+        const imagePath = path.join(__dirname, ...segments.slice(1));
+
+        if (fs.existsSync(imagePath)) {
+            return res.sendFile(imagePath);
+        }
+
+        const imageResponse = await axios.get(originalImage.href, { responseType: 'arraybuffer' });
+        
+        const imageBuffer = imageResponse.data;
+        const metadata = await sharp(imageBuffer).metadata();
+        
+        const resizedLogoBuffer = await sharp(logoPath)
+            .resize({ width: Math.round(metadata.width / 5) })
+            .toBuffer();
+
+        const outputBuffer = await sharp(imageBuffer)
+            .composite([
+                {
+                    input: resizedLogoBuffer,
+                    gravity: 'center',
+                    blend: 'overlay',
+                },
+            ])
+            .jpeg({ quality: 60 })
+            .toBuffer();
+
+        const directoryPath = path.dirname(imagePath);
+        fs.mkdirSync(directoryPath, { recursive: true });
+        fs.writeFileSync(imagePath, outputBuffer);
+        
+        return res.sendFile(imagePath);
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Server Error');
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
